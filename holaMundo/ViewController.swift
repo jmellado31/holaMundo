098a492783077dc0411cc134f944b232a75c5e0e
Juan Mellado <jmellado31@gmail.com>
2018-10-09 18:27:56 -0300

    Initial Commit

---
+++ holaMundo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  holaMundo
+//
+//  Created by Juan Carlos Mellado Rioseco on 10/9/18.
+//  Copyright © 2018 JM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
